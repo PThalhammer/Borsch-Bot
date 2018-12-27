7c6d97db3f0caee6fab9c677ec8e82c952c715b0
Philipp Thalhammer <philippthalhammer@web.de>
2018-12-27 07:50:32 +0100

    IGNORE keys

--- daily_countdown.py
+++ daily_countdown.py
@@ -6,16 +6,15 @@ import datetime
 import random
 
 
-consumer_key = 'QQo8N56cfZYN1qT0pEDWKfhDR'
-consumer_secret = 'edsr1pvh8ghuNDvavYBlat4mI0OOXLVE1y7iUMFX3r7Fl8Ewpd'
-access_token = '1066107262006104065-7tH86y2z0cvbp6WOUExzKj18fUzhaf'
-access_token_secret = 'qPKefmkz0uYR8mzHyfHJNVpfKpnWdVpBZSwyqic9y2XCa'
+import borschkeys
+import testkeys
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 today =  datetime.date.today()
-partytime =  datetime.date(2018,12, 25) 
+partytime =  datetime.date(2019,12,25) 
 
 days = partytime-today
 #print(days.days)
