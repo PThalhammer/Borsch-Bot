0a8079c681895baaae3335e73715dcb3842e0c9c
Philipp Thalhammer <philippthalhammer@web.de>
2018-12-28 01:05:24 +0100

    Implement reply to mentions

--- daily_countdown.py
+++ daily_countdown.py
@@ -4,7 +4,7 @@ import tweepy
 import time
 import datetime
 import random
-
+from math import * 
 
 #from borschkeys import *
 from testkeys import *
@@ -23,20 +23,25 @@ days = partytime-today
 user = api.me()
 #print(user.id)
 
-tw_nu = random.randint(1,6)
+tw_nu = random.randint(1,8)
+tw_nu =7
 
 if   (tw_nu == 1):
-    tweet = "Wow! Nur noch " + str(days.days) + " Tage bis zum Borsch Fest!!!"
+    tweet = "Wow! Nur noch " + str(days.days) + " Tage bis zum Borsch Fest!!! \n #BF19"
 elif (tw_nu == 2):
     tweet = "Hey, In " + str(days.days) + " Tagen ist Borsch Fest!"
 elif (tw_nu == 3):
-    tweet = "Schon gehört?! In " + str(days.days) + " Tagen ist Borsch Fest!!!"
+    tweet = "Schon gehört?! In " + str(days.days) + " Tagen ist wieder Borsch Fest!!!"
 elif (tw_nu == 4):
-    tweet = "Borsch Fest ist in "+str(days.days) + " Tagen!"
+    tweet = "Borsch Fest ist in "+str(days.days) + " Tagen! \n  #BF19"
 elif (tw_nu == 5):
     tweet = "Man mag es kaum glauben, aber Борщ-фестиваль ist in " + str(days.days) + " Tagen!!"
+elif (tw_nu == 6):
+    tweet = "Schreibt's euch in den Kalender: Am 25.12.19 ist wieder Borsch Fest!"
+elif (tw_nu == 7):
+    tweet = "Weniger als "+str(ceil(days.days/7))+" Wochen!"
 else:
-    tweet = str(days.days) + "  дня до фестиваля Борща!!!"
+    tweet = str(days.days) + "  дня до фестиваля Борща!!!  \n #BF19"
 
 try:
    api.update_status(tweet)
---
+++ reply_mentions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import tweepy
+import time
+import datetime
+import time
+import random
+
+
+#from borschkeys import *
+from testkeys import *
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+mentions = []
+
+with open('mentions.txt') as my_file:
+            mentions = my_file.read().split(',')
+#	    mentions = my_file.readlines()
+
+print(mentions)
+            
+user = api.me()
+
+new_mentions=api.mentions_timeline()
+
+for tweet in new_mentions:
+            if str(tweet.id) in mentions: continue
+            print(tweet.id)
+            msg = "@"+str(tweet.author.screen_name)+" Привет! Leider gibt's noch nichts neues zum Borsch Fest. Falls du's jedoch nicht abwarten kannst, hier ein  Rezept zum nachkochen: xchttps://tinyurl.com/y8l3k346 "
+            api.update_status(msg,in_reply_to_status_id=tweet.id)
+            mentions.append(str(tweet.id))
+
+print(mentions)
+            
+with open('mentions.txt', 'w') as f:
+    for item in mentions:
+        f.write(str(item)+",")
+	
