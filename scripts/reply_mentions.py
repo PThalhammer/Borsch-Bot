f1ba41039f235b4b263fbb7df63f8c21ce51f09b
Philipp Thalhammer <philippthalhammer@web.de>
2018-12-28 19:35:43 +0100

    Stuff

--- scripts/reply_mentions.py
+++ scripts/reply_mentions.py
@@ -18,7 +18,7 @@ api = tweepy.API(auth)
 
 mentions = []
 
-with open('../etc/mentions.txt') as my_file:
+with open("../etc/mentions.txt") as my_file:
             mentions = my_file.read().split(',')
 #	    mentions = my_file.readlines()
 
@@ -31,13 +31,13 @@ new_mentions=api.mentions_timeline()
 for tweet in new_mentions:
             if str(tweet.id) in mentions: continue
             print(tweet.id)
-            msg = "@"+str(tweet.author.screen_name)+" Привет! Leider gibt's noch nichts neues zum Borsch Fest. Falls du's jedoch nicht abwarten kannst, hier ein  Rezept zum nachkochen: xchttps://tinyurl.com/y8l3k346 "
+            msg = "@"+str(tweet.author.screen_name)+" Привет! Leider gibt's noch nichts neues zum Borsch Fest. Falls du's jedoch nicht abwarten kannst, hier ein  Rezept zum nachkochen: https://tinyurl.com/y8l3k346 "
             api.update_status(msg,in_reply_to_status_id=tweet.id)
             mentions.append(str(tweet.id))
 
 print(mentions)
             
-with open('../etc/mentions.txt', 'w') as f:
+with open("../etc/mentions.txt", 'w') as f:
     for item in mentions:
         f.write(str(item)+",")
 	
