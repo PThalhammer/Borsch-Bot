65ad648b0d40331b8010d6689a7b52213b5b08ce
Philipp Thalhammer <borschtsch.bot@gmail.com>
2020-06-26 13:41:49 +0200

    Fixed dta

--- scripts/daily_countdown.py
+++ scripts/daily_countdown.py
@@ -14,7 +14,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 today =  datetime.date.today()
-partytime =  datetime.date(2019,12,25) 
+partytime =  datetime.date(2020,12,26) 
 
 days = partytime-today
 #print(days.days)
@@ -23,28 +23,28 @@ days = partytime-today
 user = api.me()
 #print(user.id)
 
-tw_nu = random.randint(1,8)
+tw_nu = random.randint(0,9)
 
 if   (tw_nu == 1):
-    tweet = "Wow! Nur noch " + str(days.days) + " Tage bis zum Borsch Fest!!! \n #BF19"
+    tweet = "Wow! Nur noch " + str(days.days) + " Tage bis zum Borsch Fest!!! \n #BF20"
 elif (tw_nu == 2):
-    tweet = "Hey, In " + str(days.days) + " Tagen ist Borsch Fest!"
+    tweet = "Hey, In " + str(days.days) + " Tagen ist Borsch Fest! \n#BF20"
 elif (tw_nu == 3):
-    tweet = "Schon gehört?! In " + str(days.days) + " Tagen ist wieder Borsch Fest!!!"
+    tweet = "Schon gehört?! In " + str(days.days) + " Tagen ist wieder Borsch Fest!!! \n#BF20"
 elif (tw_nu == 4):
-    tweet = "Borsch Fest ist in "+str(days.days) + " Tagen! \n  #BF19"
+    tweet = "Borsch Fest ist in "+str(days.days) + " Tagen! \n  #BF20"
 elif (tw_nu == 5):
-    tweet = "Man mag es kaum glauben, aber Борщ-фестиваль ist in " + str(days.days) + " Tagen!!"
+    tweet = "Man mag es kaum glauben, aber Борщ-фестиваль ist in " + str(days.days) + " Tagen!! \n#BF20"
 elif (tw_nu == 6):
-    tweet = "Schreibt's euch in den Kalender: Am 25.12.19 ist wieder Borsch Fest!"
+    tweet = "Schreibt's euch in den Kalender: Am 26.12.20 ist wieder Borsch Fest! \n#BF20"
 elif (tw_nu == 7):
-    tweet = "Weniger als "+str(ceil(days.days/7))+" Wochen!"
+    tweet = "Weniger als "+str(ceil(days.days/7))+" Wochen! \n#BF20"
 elif (tw_nu == 8):
-    tweet = "Leute, nicht vergessen: Noch "+str(days.days)+" Tage zum großen Fest!  \n #BF19"
-elif (tw_nu == 4):
-    tweet = "For out international friends: Don't miss the Borsch-Party in "+str(days.days) + " days! \n  #BF19"
+    tweet = "Leute, nicht vergessen: Noch "+str(days.days)+" Tage zum großen Fest!  \n #BF20"
+elif (tw_nu == 9):
+    tweet = "For out international friends: Don't miss the Borsch-Party in "+str(days.days) + " days! \n  #BF20"
 else:
-    tweet = str(days.days) + "  дня до фестиваля Борща!!!  \n #BF19"
+    tweet = str(days.days) + "  дня до фестиваля Борща!!!  \n #BF20"
 
 
     
